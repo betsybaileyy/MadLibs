6b82f38433b249dccf1f11c117596c27eb2da43c
betsybaileyy <betsy.bailey80@gmail.com>
2018-10-29 15:23:07 -0700

    have the content for the madlibs in place - halloween style

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 
-def madlib():
+def madlib_go():
     print("Welcome to MadLibs")
-    noun = user_input("Enter a noun ")
-    verb = user_input("Enter a verb ")
-    adverb = user_input("Enter an adverb ")
-    adjective = user_input("Enter an adjective ")
-    print("This weekend I saw a " + noun + verb + "that was " + adverb + " " + adjective + ".")
+    emphasis_adjective = user_input("Enter an emphasis adjective: ")
+    article_of_clothing = user_input("Enter an article of clothing: ")
+    verb_one = user_input("Enter a verb: ")
+    fictional_city = user_input("Enter a fictional city: ")
+    adjective_one = user_input("Enter an adjective: ")
+    plural_proper_noun = user_input("Enter a plural proper noun:  ")
+    noun_one = user_input("Enter a verb: ")
+    noun_two = user_input("Enter a noun:  ")
+    food = user_input("Enter a food: ")
+    beverage = user_input("Enter a beverage: ")
+    adjective_two = user_input("Enter an adjective: ")
+    noun_three = user_input("Enter a noun: ")
+    verb_two = user_input("Enter a verb: ")
+    adjective_three = user_input("Enter an adjective: ")
+    print("Happy Halloween! It’s time to get " + emphasis_adjective + " spooky. Get your " + article_of_clothing + " on and prepare to " + verb_one + " around the town. Every year on October 31st, " + fictional_city + " hosts the " + adjective_one + " party and " + plural_proper_noun + " never forget to come. It is important to greet everyone by saying ‘ " + noun_one + " or " + noun_two + " .’ We will eat lots of " + food + " and drink " + beverage + " . Bring your finest " + adjective_two + noun_three + " and don’t forget to " + verb_two + " because this is the most " + adjective_three + " day of the year.")
 
 def user_input(prompt):
     user_input = input(prompt)
     return user_input
 
 
-madlib()
+madlib_go()
