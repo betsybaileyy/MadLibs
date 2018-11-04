1a49b7baff4b268b5fd90c8351f41d3f8a15b8e2
betsybaileyy <betsy.bailey80@gmail.com>
2018-11-04 12:23:18 -0800

    adding readme

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Welcome to SPOOKY MadLibs</h1>
+<h3>This application is written in Python 3 and utilizes
+simple functions and variable declarations to create a MadLibs
+program for the user to interact with.</h3>
+<h5>To run this program clone into this repository and pull up the MadLibs
+directory in your terminal. Run the command "./main.py python3" (exclude quotation marks)
+and follow the prompts.</h5>
+
+<h1>Happy Halloween!</h1>
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 def madlib_go():
     print("Welcome to MadLibs")
-    print("Please read the the paragraph below and then fill in the prompts to complete the spooky Halloween story!")
-    print("Happy Halloween! It’s time to get (emphasis adjective) spooky. Get your (article of clothing) on and prepare to (verb) around the town. Every year on October 31st, (fictional city) hosts the (adjective) party and (plural proper noun) never forget to come. It is important to greet everyone by saying “ (noun) or (noun) “. We will eat lots of (food) and drink (beverage). Bring your finest (adjective) (noun) and don’t forget to (verb) because this is the most (adjective) day of the year. ")
+    print("Please fill in the prompts to complete the spooky Halloween story!")
+
+
+    # asking user input
     emphasis_adjective = user_input("Enter an emphasis adjective: ")
     article_of_clothing = user_input("Enter an article of clothing: ")
     verb_one = user_input("Enter a verb: ")
@@ -17,7 +19,7 @@ def madlib_go():
     noun_three = user_input("Enter a noun: ")
     verb_two = user_input("Enter a verb: ")
     adjective_three = user_input("Enter an adjective: ")
-    print("Happy Halloween! It’s time to get " + emphasis_adjective + " spooky. Get your " + article_of_clothing + " on and prepare to " + verb_one + " around the town. Every year on October 31st, " + fictional_city + " hosts the " + adjective_one + " party and " + plural_proper_noun + " never forget to come. It is important to greet everyone by saying ‘ " + noun_one + " or " + noun_two + " .’ We will eat lots of " + food + " and drink " + beverage + " . Bring your finest " + adjective_two + noun_three + " and don’t forget to " + verb_two + " because this is the most " + adjective_three + " day of the year.")
+    print("Happy Halloween! It’s time to get " + emphasis_adjective + " spooky. Get your " + article_of_clothing + " on and prepare to " + verb_one + " around the town. Every year on October 31st, " + fictional_city + " hosts the " + adjective_one + " party and " + plural_proper_noun + " never forget to come. It is important to greet everyone by saying ‘ " + noun_one + " or " + noun_two + " .’ We will eat lots of " + food + " and drink " + beverage + " . Bring your finest " + adjective_two + " " + noun_three + " and don’t forget to " + verb_two + " because this is the most " + adjective_three + " day of the year.")
 
 def user_input(prompt):
     user_input = input(prompt)
@@ -25,3 +27,12 @@ def user_input(prompt):
 
 
 madlib_go()
+
+    #
+    #
+    # print(""" Happy Halloween! It’s time to get
+    # (emphasis adjective) spooky. Get your (article of clothing)
+    # on and prepare to (verb) around the town. Every year on October 31st,
+    # (fictional city) hosts the (adjective) party and (plural proper noun) never forget to come.
+    # It is important to greet everyone by saying “ (noun) or (noun) “. We will eat lots of (food) and drink (beverage).
+    # Bring your finest (adjective) (noun) and don’t forget to (verb) because this is the most (adjective) day of the year.""")
